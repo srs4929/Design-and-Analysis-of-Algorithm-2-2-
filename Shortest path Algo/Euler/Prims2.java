c1e773b0756e8eb9cb50a0fe82b044a7874af1be
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-05-18 07:59:55 +0600

    Update Prims2.java

--- Shortest path Algo/Euler/Prims2.java
+++ Shortest path Algo/Euler/Prims2.java
@@ -1,90 +1,86 @@
 import java.util.*;
-
-public class Main {
-
-    static class Edge {
-        int u;
+public class Main{
+    
+    static class Edge{
+        
         int v;
         int w;
-        Edge(int u, int v, int w) {
-            this.u = u;
-            this.v = v;
-            this.w = w;
+        Edge(int v,int w)
+        {
+           
+            this.v=v;
+            this.w=w;
         }
+        
     }
-
-    static class Graph {
-        List<Edge> edge;
+    static class Graph{
+        
         int v;
-
-        Graph(int v) {
-            this.v = v;
-            this.edge = new ArrayList<>();
+        List<List<Edge>>adj;
+        Graph(int v)
+        {    this.v=v;
+            this.adj=new ArrayList<>();
+            for(int i=0;i<v;i++) adj.add(new ArrayList<>());
         }
-
-        public void addedge(int u, int v, int w) {
-            edge.add(new Edge(u, v, w));
+        public void addedge(int u,int v,int w)
+        {
+            adj.get(u).add(new Edge(v,w));
+            adj.get(v).add(new Edge(u,w));
         }
-
-        void printmst(int s) {
-            int[] p = new int[v];
-            int[] key = new int[v];
-            boolean[] mst = new boolean[v];
-            PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparing(e -> e.w));
-
-            Arrays.fill(key, Integer.MAX_VALUE);
-            Arrays.fill(p, -1);
-
-            key[s] = 0;
-            pq.add(new Edge(-1, s, 0));
-
-            while (!pq.isEmpty()) {
-                Edge cur = pq.poll();
-                int u = cur.v;
-
-                if (mst[u]) continue;
-
-                mst[u] = true;
-
-                for (Edge e : this.edge) {   // loop over all edges
-                    if (e.u == u && !mst[e.v] && e.w < key[e.v]) {
-                        key[e.v] = e.w;
-                        p[e.v] = u;
-                        pq.add(new Edge(u, e.v, e.w));
+        public void result(int s)
+        {
+            int [] key=new int[v];
+            Arrays.fill(key,Integer.MAX_VALUE);
+            boolean [] mst=new boolean[v];
+            key[s]=0;
+            PriorityQueue<int[]>pq=new PriorityQueue<>(Comparator.comparing(a->a[1]));
+            int total=0;
+            pq.add(new int []{s,0});
+            while(!pq.isEmpty())
+            {
+                int []curr=pq.poll();
+                int u=curr[0];
+                if(mst[u]) continue;
+                mst[u]=true;
+                total+=curr[1];
+                for(Edge e:adj.get(u))
+                {
+                    if(!mst[e.v] && e.w<key[e.v])
+                    {
+                        key[e.v]=e.w;
+                        pq.add(new int []{e.v,e.w});
                     }
-                    if (e.v == u && !mst[e.u] && e.w < key[e.u]) {
-                        key[e.u] = e.w;
-                        p[e.u] = u;
-                        pq.add(new Edge(u, e.u, e.w));
-                    }
-                }
-            }
-
-            int total = 0;
-            for (int i = 0; i < v; i++) {
-                if (p[i] != -1) {
-                    System.out.println(p[i] + " - " + i);
-                    total += key[i];
                 }
             }
             System.out.println(total);
+            
+            
         }
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int V = sc.nextInt();
-        int E = sc.nextInt();
-        Graph graph = new Graph(V);
-
-        for (int i = 0; i < E; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int weight = sc.nextInt();
-            graph.addedge(u, v, weight);
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int v=sc.nextInt();
+        int e=sc.nextInt();
+        Graph g=new Graph(v);
+        for(int i=0;i<e;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int w=sc.nextInt();
+            g.addedge(a-1, b-1, w);
         }
-
-        int start = sc.nextInt();
-        graph.printmst(start);
+        int s=sc.nextInt();
+        g.result(s-1);
     }
 }
+        
+                  
+    
+
+               
+                
+            
+            
+            
+  
