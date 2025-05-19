f29ee488986d89a19882f31a0d050f7fdfe311cf
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-05-19 17:25:34 +0600

    Update kstop.java

--- Shortest path Algo/Problem/kstop.java
+++ Shortest path Algo/Problem/kstop.java
@@ -1,3 +1,9 @@
+/*There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.*/
+
+
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.*;
@@ -72,3 +78,9 @@ public class kstop {
     }
 
 }
+
+/*Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.*/
