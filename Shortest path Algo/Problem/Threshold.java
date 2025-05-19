3ebe4f776eb7ef506e887b0e78ca445069a79c8c
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-05-19 16:12:07 +0600

    Update Threshold.java

--- Shortest path Algo/Problem/Threshold.java
+++ Shortest path Algo/Problem/Threshold.java
@@ -2,7 +2,8 @@
 
 Return the city with the smallest number of cities that are reachable through some path and whose distance is at most distanceThreshold, If there are multiple such cities, return the city with the greatest number.
 
-Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.*/
+Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.
+Leetcode: Find the city with smallest numbers at a threshold distance*/
 import java.util.*;
 
 public class Threshold {
